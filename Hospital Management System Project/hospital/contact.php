d9db2b8fb1f6cf16fac6cb9fac446c7dd402082f
WithMaro <marodaniel.2605@gmail.com>
2021-11-02 09:11:17 -0500

    Actividad 1

---
+++ Hospital Management System Project/hospital/contact.php
@@ -0,0 +1,110 @@
+<?php
+include_once('hms/include/config.php');
+if(isset($_POST['submit']))
+{
+$name=$_POST['fullname'];
+$email=$_POST['emailid'];
+$mobileno=$_POST['mobileno'];
+$dscrption=$_POST['description'];
+$query=mysqli_query($con,"insert into tblcontactus(fullname,email,contactno,message) value('$name','$email','$mobileno','$dscrption')");
+echo "<script>alert('Your information succesfully submitted');</script>";
+echo "<script>window.location.href ='contact.php'</script>";
+
+}
+
+
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Contáctanos</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+		<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<!--start-wrap-->
+		
+			<!--start-header-->
+			<div class="header">
+				<div class="wrap">
+				<!--start-logo-->
+				<div class="logo">
+		<a href="index.html" style="font-size: 30px;">Sistema de gestión hospitalaria</a> 
+				</div>
+				<!--end-logo-->
+				<!--start-top-nav-->
+				<div class="top-nav">
+					<ul>
+						<li><a href="index.html">Inicio</a></li>
+					
+						<li class="active"><a href="contact.php">Contactanos</a></li>
+					</ul>					
+				</div>
+				<div class="clear"> </div>
+				<!--end-top-nav-->
+			</div>
+			<!--end-header-->
+		</div>
+		    <div class="clear"> </div>
+		   <div class="wrap">
+		   	<div class="contact">
+		   	<div class="section group">				
+				<div class="col span_1_of_3">
+					
+      			<div class="company_address">
+				     	<h2>Dirección del hospital:</h2>
+						    	<p>Av. Goyeneche, Arequipa 04001</p>
+						   		<p>Perú</p>
+				   		<p>Phone:(054) 231313</p>
+				   	
+				   </div>
+				</div>				
+				<div class="col span_2_of_3">
+				  <div class="contact-form">
+				  	<h2>Contactanos</h2>
+					    <form name="contactus" method="post">
+					    	<div>
+						    	<span><label>Nombre</label></span>
+						    	<span><input type="text" name="fullname" required="true" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>Correo Electrónico</label></span>
+						    	<span><input type="email" name="emailid" required="ture" value=""></span>
+						    </div>
+						    <div>
+						     	<span><label>Numero de Telefono</label></span>
+						    	<span><input type="text" name="mobileno" required="true" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>Descripción</label></span>
+						    	<span><textarea name="description" required="true"> </textarea></span>
+						    </div>
+						   <div>
+						   		<span><input type="submit" name="submit" value="Enviar"></span>
+						  </div>
+					    </form>
+				    </div>
+  				</div>				
+			  </div>
+			  	 <div class="clear"> </div>
+	</div>
+	<div class="clear"> </div>
+			</div>
+	      <div class="clear"> </div>
+		   <div class="footer">
+		   	 <div class="wrap">
+		   	<div class="footer-left">
+		   			<ul>
+						<li><a href="index.html">Inicio</a></li>
+						
+						<li><a href="contact.php">Contactanos</a></li>
+					</ul>
+		   	</div>
+		  
+		   	<div class="clear"> </div>
+		   </div>
+		   </div>
+		<!--end-wrap-->
+	</body>
+</html>
+
